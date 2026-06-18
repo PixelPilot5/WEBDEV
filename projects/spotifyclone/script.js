246e0a839f645093b6d569373085174f550aad00
PixelPilot5 <pixelpilot555@gmail.com>
2026-06-18 23:34:31 +0530

    spoify clone

--- projects/spotifyclone/script.js
+++ projects/spotifyclone/script.js
@@ -10,3 +10,16 @@ btn.addEventListener('click', () => {
     behavior: 'smooth'   // Makes the slide animation fluid
   });
 });
+
+const container2 = document.getElementById('card-container2');
+const btn2 = document.getElementById('circle-nav-btn2');
+
+btn2.addEventListener('click', () => {
+  // Define how far you want to scroll on each click (e.g., 200 pixels)
+  const scrollAmount = 390;
+
+  container2.scrollBy({
+    left: scrollAmount,   // Positive number scrolls right; negative scrolls left
+    behavior: 'smooth'   // Makes the slide animation fluid
+  });
+});
